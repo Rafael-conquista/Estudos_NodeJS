5336c2717fce240153ea1b8e79bc142694799ff6
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-06-23 20:07:06 -0300

    feat: implementa rota e métodos para alunos

--- projeto_api_express2/src/controllers/Aluno.js
+++ projeto_api_express2/src/controllers/Aluno.js
@@ -5,6 +5,88 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['faltando id'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não está cadastrado'],
+        });
+      }
+      return res.status(200).json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = Aluno.create(req.body);
+      return res.status(200).json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['faltando id'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não está cadastrado'],
+        });
+      }
+
+      const alunoAtualizado = Aluno.update(req.body);
+
+      return res.status(200).json(alunoAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['faltando id'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não está cadastrado'],
+        });
+      }
+
+      await Aluno.destroy();
+      return res.json('Aluno deletado com sucesso');
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- projeto_api_express2/src/routes/aluno.js
+++ projeto_api_express2/src/routes/aluno.js
@@ -4,5 +4,9 @@ import alunoController from '../controllers/Aluno';
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.post('/', alunoController.store);
+router.put('/:id', alunoController.update);
+router.delete('/:id', alunoController.delete);
+router.get('/:id', alunoController.show);
 
 export default router;

f682950919507c252ba8e7fd4d38d8fa3fd22ce9
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-08-03 20:09:15 -0300

    start unit test studies

--- projeto_api_express2/app.js
+++ projeto_api_express2/app.js
@@ -6,6 +6,7 @@ import home from './src/routes/home';
 import user from './src/routes/user';
 import token from './src/routes/token';
 import alunos from './src/routes/aluno';
+import fotos from './src/routes/foto';
 import './src/database';
 
 class App {
@@ -25,6 +26,7 @@ class App {
     this.app.use('/users', user);
     this.app.use('/tokens', token);
     this.app.use('/alunos', alunos);
+    this.app.use('/fotos', fotos);
   }
 }
 

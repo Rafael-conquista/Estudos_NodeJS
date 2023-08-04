014e0ef5be78ed342111b5fe44917c97c91a414f
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-08-04 19:36:46 -0300

    defining behaviors and fixing the code

--- testes/code/basic-testing-frontend/src/math.test.js
+++ testes/code/basic-testing-frontend/src/math.test.js
@@ -2,7 +2,35 @@ import { it, expect } from 'vitest'
 import { add } from './math'
 
 
-it('should summariz all number values in an array', () =>{
-    const result = add([1,2,3])
-    expect(result).toBe(6)
+it('should summarize all number values in an array', () =>{
+    //arrange 
+    const numbers = [1,2,3]
+    //act
+    const result = add(numbers)
+    //Assert
+    const expectedResult = numbers.reduce(
+        (prevValue,currentValue) => prevValue + currentValue, 
+        0
+    )
+    expect(result).toBe(expectedResult)
+})
+
+it('should yield NaN if at least one invalid number is provided', () =>{
+    const inputs = ['invalid value', 1]
+
+    const result = add(inputs)
+
+    expect(result).toBeNaN()
+})
+
+it('should yield a correct sum if an array of numeric strings values is provided', () => {
+    const numbers = ['1', '2']
+
+    const result = add(numbers)
+
+    const expectedResult = numbers.reduce(
+        (prevValue,currentValue) => +prevValue + +currentValue, 
+        0
+    )
+    expect(result).toBe(expectedResult)
 })

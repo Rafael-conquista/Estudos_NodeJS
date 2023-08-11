681b9b9729f1a065dd3adc4dd76a3da6855cf30b
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-08-11 18:19:32 -0300

    more tests exercises and multiple expetations

--- testes/code/basic-testing-frontend/src/util/numbers.test.js
+++ testes/code/basic-testing-frontend/src/util/numbers.test.js
@@ -6,9 +6,15 @@ it('should return a number, not a string',() => {
     const result = transformToNumber(number)
     expect(result).toBeTypeOf('number')
 })
+//that's possible to make multiple asertions
 
 it('should yield Nan for non-transformable values', () => {
     const  input = 'invalid'
+    const input2 = {}
     const result = transformToNumber(input)
+    const result2 = transformToNumber(input2)
     expect(result).toBeNaN()
+    expect(result2).toBeNaN()
 })
+// here we have the same expetation being tested twice,
+// but each one is obout a different scenarios

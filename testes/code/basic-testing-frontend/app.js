fed408d8dfeef4695da636f39cd6946d1b79cb3e
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-08-15 19:39:12 -0300

    refactoring app.js

--- testes/code/basic-testing-frontend/app.js
+++ testes/code/basic-testing-frontend/app.js
@@ -1,43 +1,12 @@
-import { extractNumbers } from './src/parser.js';
-import {
-  validateStringNotEmpty,
-  validateNumber,
-} from './src/util/validation.js';
-import { add } from './src/math.js';
-import { transformToNumber } from './src/util/numbers.js';
+export function extractNumbers(formData) {
+  const num1Input = formData.get('num1');
+  const num2Input = formData.get('num2');
 
-const form = document.querySelector('form');
-const output = document.getElementById('result');
-
-function formSubmitHandler(event) {
-  event.preventDefault();
-  const formData = new FormData(form);
-  const numberInputs = extractNumbers(formData);
-
-  let result = '';
-  
-  try {
-    const numbers = [];
-    for (const numberInput of numberInputs) {
-      validateStringNotEmpty(numberInput);
-      const number = transformToNumber(numberInput);
-      validateNumber(number);
-      numbers.push(number);
-    }
-    result = add(numbers).toString();
-  } catch (error) {
-    result = error.message;
-  }
-
-  let resultText = '';
-
-  if (result === 'invalid') {
-    resultText = 'Invalid input. You must enter valid numbers.';
-  } else if (result !== 'no-calc') {
-    resultText = 'Result: ' + result;
-  }
-
-  output.textContent = resultText;
+  return [num1Input, num2Input];
 }
 
-form.addEventListener('submit', formSubmitHandler);
+export function extractEnteredNumberValues(form) {
+  const formData = new FormData(form);
+  const numberInputs = extractNumbers(formData);
+  return numberInputs;
+}
